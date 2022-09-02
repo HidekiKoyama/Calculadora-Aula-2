a289e762750267ab660c7d23aaad89d3456a03c7
Hideki <111573020+HidekiKoyama@users.noreply.github.com>
2022-09-01 21:13:27 -0300

    Create simple.cs

---
+++ simple.cs
@@ -0,0 +1,60 @@
+using System;
+
+namespace hideki.aula.definir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //                 Variaveis
+
+            //                                      Terceiro Programa - (CALCULADORA)
+
+
+            //Variaveis
+
+            
+            string operacao;
+            double valor1, valor2;
+
+            Console.WriteLine("**********Sua calculadora em C#************");
+            Console.WriteLine("Diga a operação que você usará.\n\tSendo + para Soma, * para multiplicação e / para divião.\n\n Digite a operação que você usará: ");
+            operacao = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine("Escolha o seu primeiro número: ");
+            valor1 = double.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.WriteLine("Digite o segundo número: ");
+            valor2 = double.Parse(Console.ReadLine());
+
+            if (operacao == "/")
+            {
+                Console.WriteLine(valor1 / valor2);
+            }
+            else if (operacao == "*")
+            {
+                Console.WriteLine(valor1 * valor2);
+            }
+            else if (operacao == "-")
+            {
+                Console.WriteLine(valor1 - valor2);
+            }
+            else if (operacao == "+")
+            {
+                Console.WriteLine(valor1 + valor2);
+            }
+            else
+            {
+                Console.WriteLine("Algo deu errado na esolha de suas operações!");
+            }
+            
+
+
+
+        }
+    }
+}
